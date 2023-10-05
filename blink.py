b05cdde2c806b4d4984c3b4286499ca21db8c232
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-10-05 00:36:07 +0000

    finnish

--- blink.py
+++ blink.py
@@ -10,51 +10,51 @@ import digitalio
 
 red = digitalio.DigitalInOut(board.GP0)
 green = digitalio.DigitalInOut(board.GP1)
-blue = digitalio.DigitalInOut(board.GP3)
+blue = digitalio.DigitalInOut(board.GP2)
 
 red.direction = digitalio.Direction.OUTPUT
 green.direction = digitalio.Direction.OUTPUT
 blue.direction = digitalio.Direction.OUTPUT
 
 while True:
-red.value = True
-green.value = False
-blue.value = False
-print("Red")
-time.sleep(1)
+    red.value = True
+    green.value = False
+    blue.value = False
+    print("Red")
+    time.sleep(1)
 
-red.value = False
-green.value = True
-blue.value = False
-print("Green")
-time.sleep(1)
+    red.value = False
+    green.value = True
+    blue.value = False
+    print("Green")
+    time.sleep(1)
 
-red.value = False
-green.value = False
-blue.value = True
-print("Blue")
-time.sleep(1)
+    red.value = False
+    green.value = False
+    blue.value = True
+    print("Blue")
+    time.sleep(1)
 
-red.value = True
-green.value = True
-blue.value = False
-print("Yellow")
-time.sleep(1)
+    red.value = True
+    green.value = True
+    blue.value = False
+    print("Yellow")
+    time.sleep(1)
 
-red.value = False
-green.value = True
-blue.value = True
-print("Cyan")
-time.sleep(1)
+    red.value = False
+    green.value = True
+    blue.value = True
+    print("Cyan")
+    time.sleep(1)
 
-red.value = True
-green.value = False
-blue.value = True
-print("Magenta")
-time.sleep(1)
+    red.value = True
+    green.value = False
+    blue.value = True
+    print("Magenta")
+    time.sleep(1)
 
-red.value = True
-green.value = True
-blue.value = True
-print("White")
-time.sleep(1)
+    red.value = True
+    green.value = True
+    blue.value = True
+    print("White")
+    time.sleep(1)
